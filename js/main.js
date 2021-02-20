10f5a2e2824f083c96f195320312bc77c9b3b7cd
keryboks <kiryusha-danchenko@mail.ru>
2021-02-20 12:02:27 +0300

    Module

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 
-
-
-
 import {getRandomIntInclusive, getRandomFloat,arrayRandElement, zeroPad, arrayRandNumber,arrayRandFeatures,arrayRandCheckout,arrayRandCheckin,arrayRandPhotos, arrayRandDescription,arrayRandTitle, unique,featuresArray} from './util.js';
 import {similarFeaturesCount,createFeatures, similarFeatures,typeArray,numbersArray,checkinArray,checkoutArray,photosArray,descriptionArray, titleArray} from './data.js';
 
