6ea001bde66f40ed4482cf52d74a975931176c36
keryboks <68223858+keryboks@users.noreply.github.com>
2021-02-27 18:27:33 +0300

    добавил файлы

---
+++ js/form.js
@@ -0,0 +1,33 @@
+let typeOfPlace = document.querySelector('#type');
+let priceOfPlace = document.querySelector('#price');
+let timeIn = document.querySelector('#timein');
+let timeOut = document.querySelector('#timeout')
+
+
+let houseInformation = {
+  bungalow: 0,
+  flat: 1000,
+  house: 5000,
+  palace: 10000,
+};
+
+
+typeOfPlace.addEventListener('change', function () {
+  priceOfPlace.min = houseInformation[typeOfPlace.value];
+  priceOfPlace.placeholder = houseInformation[typeOfPlace.value];
+  console.log(priceOfPlace.min);
+});
+
+
+timeIn.addEventListener('change', function () {
+   if(timeIn.value){
+  timeOut.value = timeIn.value;
+   };
+});
+
+timeOut.addEventListener('change', function () {
+  if(timeOut.value){
+  timeIn.value = timeOut.value;
+  }
+});
+export { typeOfPlace, priceOfPlace, timeIn, timeOut, houseInformation };
